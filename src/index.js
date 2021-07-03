e32292f0a55d80a3fc2c4286c9c877c9bc4dede0
Benjawan1999 <86357740+Benjawan1999@users.noreply.github.com>
2021-07-03 22:46:47 +0700

    commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import book from './books.json'
+
+import './index.css'
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.getElementById('card')
+    const books = book.data
+    console.log(books);
+    
+    books.forEach(bookk => {
+        let cardDOM = buildCardDOM(bookk)
+        cards.insertAdjacentHTML('beforeend', cardDOM)
+    })
+})
+
+function buildCardDOM(bookk) {
+    let photos = book.image_base_url
+
+    return (
+        `<div class="cards">
+            <img src="${photos.concat(`${bookk.ImageURL}`)}" alt="">
+            <h2 class="Title">${bookk.Title}</h2>
+            <h4 class="Authors">${bookk.Authors}</h4>
+            <P class="Format">${bookk.Format}</P>
+        </div>`
+    )
+}
+
